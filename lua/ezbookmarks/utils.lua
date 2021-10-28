aa9af9065f27351b3e8b126629ce73ae51b1a552
Aris Nikoletopoulos <a.nikoletopoulos@gmail.com>
2021-10-28 20:16:28 +0300

    Moved utility functions to utils.

---
+++ lua/ezbookmarks/utils.lua
@@ -0,0 +1,62 @@
+-----------------------------
+-- EzBookmarks by lifer0se --
+-----------------------------
+
+local bookmark_file = vim.fn.stdpath('data') .. '/ezbookmarks.txt'
+local M = {}
+
+M.get_lines_from_bookmark_file = function ()
+  local lines = {}
+  if vim.fn.filereadable(bookmark_file) == 0 then
+    return lines
+  end
+
+  local has_changes = false
+  for line in io.lines(bookmark_file) do
+    if vim.fn.filereadable(line) == 1 or vim.fn.isdirectory(line) == 1 then
+      lines[#lines + 1] = line
+    else
+      has_changes = true
+    end
+  end
+
+  if has_changes then
+    local n = ""
+    for k, v in pairs(lines) do
+      n = n .. v .. '\n'
+    end
+    local f = io.open(bookmark_file, 'w')
+    f:write(n)
+    f:close()
+  end
+
+  return lines
+end
+
+
+M.bookmark_exists = function (path)
+  local lines = M.get_lines_from_bookmark_file()
+  for k, v in pairs(lines) do
+    if (vim.fn.filereadable(v) == 1 or vim.fn.isdirectory(v) == 1) and v == path then
+      return 1
+    elseif vim.fn.filereadable(v) == 1 then
+      for k, v in pairs(lines) do
+        if string.sub(v, #v) == "/" and string.match(path, v) and v ~= path then
+          return -1
+        end
+      end
+    elseif vim.fn.isdirectory(v) == 1 and string.match(path, v) then
+      return -1
+    end
+  end
+  return 0
+end
+
+
+M.get_path_from_config = function (config)
+  for token in string.gmatch(config, "[^%s]+") do
+    return token
+  end
+end
+
+return M
