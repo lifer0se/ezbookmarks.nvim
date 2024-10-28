5cfb1fcfaebbfcfaa736922fd216721ecb2505e3
Aris Nikoletopoulos <a.nikoletopoulos@gmail.com>
2024-10-29 01:11:58 +0200

    Added bookmark ignore. Optimised & refactored project.

--- lua/ezbookmarks/init.lua
+++ lua/ezbookmarks/init.lua
@@ -4,230 +4,244 @@
 
 local pickers = require "telescope.pickers"
 local finders = require "telescope.finders"
-local previewers = require "telescope.previewers"
-local make_entry = require "telescope.make_entry"
 local conf = require("telescope.config").values
 local actions = require "telescope.actions"
 local action_state = require "telescope.actions.state"
 local utils = require "ezbookmarks.utils"
 local bookmark_file = vim.fn.stdpath('data') .. '/ezbookmarks.txt'
+local ignore_file = vim.fn.stdpath('data') .. '/ezbookmarks.ign'
 
 local cwd_on_open
-local use_bookmark_dir
 local open_new_tab
 
 local M = {}
 
 
-M.setup = function (opts)
-  cwd_on_open = opts.cwd_on_open
-  use_bookmark_dir = opts.use_bookmark_dir
-  open_new_tab = opts.open_new_tab
+M.setup = function(opts)
+	cwd_on_open = opts.cwd_on_open
+	open_new_tab = opts.open_new_tab
 end
 
 
 M.AddBookmark = function()
-  local path = vim.fn.expand("%:p")
-  if path == " " or path == "" or path == nil then
-    return
-  end
-  if vim.fn.filereadable(path) == 0 then
-    return
-  end
+	local path = utils.get_path("Add to bookmark list")
+	if path == nil then
+		return
+	end
 
-  local b = utils.bookmark_exists(path)
-  if b == 1 then
-    print("File \"" .. path .. "\" already exists in the bookmark list.")
-    return
-  elseif b == -1 then
-    print("File \"" .. path .. "\" is nested in a bookmark directory.")
-    return
-  end
+	local ignore_list = utils.get_safe_list_from_file(ignore_file)
+	local ignore_exists = utils.path_exists_in_list(path, ignore_list)
+	if ignore_exists == 1 then
+		local response = utils.get_user_response("Path \"" .. path .. "\" exists in the ignore list. Remove it? [y/N] ",
+			"")
+		if response:lower() == "y" or response:lower() == "yes" then
+			utils.remove_path_from_list(path, ignore_list, ignore_file)
+			print("Removed \"" .. path .. "\" from ignore list.")
+		end
+		return
+	end
 
-  local f = io.open(bookmark_file, 'a+')
-  f:write(path .. '\n')
-  f:close()
-  print("Added file \"" .. path .. "\" to bookmarks.")
+	local bookmark_list = utils.get_safe_list_from_file(bookmark_file)
+	local bookmark_exists = utils.path_exists_in_list(path, bookmark_list)
+	if bookmark_exists == 1 then
+		print("Path \"" .. path .. "\" already exists in the bookmark list.")
+		return
+	elseif bookmark_exists == -1 then
+		print("Path \"" .. path .. "\" is nested in another directory in the bookmark list.")
+		return
+	end
+
+	if vim.fn.isdirectory(path) then
+		utils.try_remove_nested_from_list(path, bookmark_list, bookmark_file)
+	end
+
+	local f = io.open(bookmark_file, 'a+')
+	f:write(path .. '\n')
+	f:close()
+	print("Added \"" .. path .. "\" to bookmarks.")
 end
 
 
-M.AddBookmarkDirectory = function ()
-  local path = vim.fn.expand("%:p:h")
-  path = vim.fn.input("Add directory: ", path)
-  vim.api.nvim_command('normal! :')
-  if vim.fn.isdirectory(path) == 0 then
-    print("Directory \"" .. path .."\" does not exist.")
-    return
-  end
-  if string.sub(path, #path) ~= '/' then
-    if (vim.fn.has("win32") == 0) then
-      path = path .. '/'
-    else
-      path = path .. '\\'
-    end
-  end
+M.AddIgnore = function()
+	local path = utils.get_path("Add to ignore list")
+	if path == nil then
+		return
+	end
 
-  local b = utils.bookmark_exists(path)
-  if b == 1 then
-    print("Directory \"" .. path .. "\" already exists in the bookmark list!")
-    return
-  elseif b == -1 then
-    print("Directory \"" .. path .. "\" is nested in another bookmark directory.")
-    return
-  end
+	local ignore_list = utils.get_safe_list_from_file(ignore_file)
+	local ignore_exists = utils.path_exists_in_list(path, ignore_list)
+	if ignore_exists == 1 then
+		print("Path \"" .. path .. "\" already exists in the ignore list.")
+		return
+	elseif ignore_exists == -1 then
+		print("Path \"" .. path .. "\" is nested in another directory in the ignore list.")
+		return
+	end
 
-  local lines = utils.get_lines_from_bookmark_file()
-  local n = ""
-  local has_changes = false
-  for k, v in pairs(lines) do
-    if not string.find(v, path) then
-      n = n .. v .. '\n'
-    else
-      has_changes = true
-    end
-  end
-  if has_changes then
-    local f = io.open(bookmark_file, 'w')
-    f:write(n)
-    f:close()
-  end
+	local bookmark_list = utils.get_safe_list_from_file(bookmark_file)
+	local bookmark_exists = utils.path_exists_in_list(path, bookmark_list)
+	if bookmark_exists == 1 then
+		local response = utils.get_user_response(
+			"Path \"" .. path .. "\" exists in the bookmark list. Remove it? [y/N] ", "")
+		if response:lower() == "y" or response:lower() == "yes" then
+			utils.remove_path_from_list(path, bookmark_list, bookmark_file)
+			utils.remove_path_from_list(path, ignore_list, ignore_file)
+			print("Removed \"" .. path .. "\" from bookmark list.")
+		end
+		return
+	elseif bookmark_exists == 0 then
+		print("Path \"" .. path .. "\" is not a nested path in the bookmark list.")
+		return
+	end
 
-  local f = io.open(bookmark_file, 'a+')
-  f:write(path .. '\n')
-  f:close()
-  print("Added directory \"" .. path .. "\" to bookmarks.")
+	if vim.fn.isdirectory(path) then
+		utils.try_remove_nested_from_list(path, ignore_list, ignore_file)
+	end
+
+	local f = io.open(ignore_file, 'a+')
+	f:write(path .. '\n')
+	f:close()
+	print("Added \"" .. path .. "\" to ignore list.")
 end
 
 
-local function open(selection)
-  if selection ~= nil then
+local function open_selection(path)
+	if path == nil then
+		return
+	end
 
-    local dir_found = false
-    if use_bookmark_dir == 1 then
-      local lines = utils.get_lines_from_bookmark_file()
-      for k, f in pairs(lines) do
-        local path = utils.sub_home_path(f)
-        if string.match(selection, path) then
-          local b = false
-          if (vim.fn.has("win32") == 0) then
-            b = string.sub(path, -1) == "/"
-          else
-            b = string.sub(path, -1) == "\\"
-          end
+	if cwd_on_open == 1 then
+		vim.cmd(':cd ' .. utils.get_path_from_file(path))
+	end
 
-          if b then
-            vim.cmd(':cd ' .. path)
-            dir_found = true
-            break
-          end
-        end
-      end
-    end
-
-    if cwd_on_open == 1 then
-      if dir_found == false then
-        vim.cmd(':cd ' .. utils.get_path_from_file(selection))
-      end
-    end
-
-    if open_new_tab == 1 then
-      vim.cmd(':tabedit ' .. selection)
-    else
-      vim.cmd(':edit ' .. selection)
-    end
-  end
+	if open_new_tab == 1 then
+		vim.cmd(':tabedit ' .. path)
+	else
+		vim.cmd(':edit ' .. path)
+	end
 end
+
 
 M.OpenBookmark = function(opts)
-  local lines = utils.get_lines_from_bookmark_file()
-  if #lines == 0 then
-    print('Bookmark list is empty.')
-    return
-  end
+	local list = utils.get_safe_list_from_file(bookmark_file)
+	if #list == 0 then
+		print('Bookmark list is empty.')
+		return
+	end
 
-  local n = {}
-  for k, v in pairs(lines) do
-    if vim.fn.filereadable(v) == 1 then
-      n[#n + 1] = utils.sub_home_path(v)
-    elseif vim.fn.isdirectory(v) == 1 then
-      local p = ""
-      if (vim.fn.has("win32") == 0) then
-        p = io.popen("find ".. v .." -type f")
-      else
-        p = io.popen("cd " .. v .. " && dir /s /b | findstr /m /c:.")
-      end
-      for f in p:lines() do
-        local tmp = string.sub(f, #v + 1, #f)
-        if not string.find(tmp, ".git") then
-          n[#n + 1] = utils.sub_home_path(f)
-        end
-      end
-    end
-  end
-  opts = opts or {}
-  pickers.new(opts, {
-    prompt_title = "Open a bookmark",
-    finder = finders.new_table {
-      results = n
-    },
-    layout_config = {
-      preview_width = 0.5,
-    },
-    sorter = conf.file_sorter(opts),
-    previewer = conf.file_previewer(opts),
-    attach_mappings = function(prompt_bufnr, map)
-      actions.select_default:replace(function()
-        actions.close(prompt_bufnr)
-        open(action_state.get_selected_entry()[1])
-      end)
-      return true
-    end,
-  }):find()
+	local ignore_list = utils.get_safe_list_from_file(ignore_file)
+	local selection = {}
+	for _, path in pairs(list) do
+		if vim.fn.filereadable(path) == 1 then
+			if utils.path_exists_in_list(path, ignore_list) ~= 0 then
+				goto continue
+			end
+			selection[#selection + 1] = utils.sub_home_path(path)
+			goto continue
+		end
+
+		local nested
+		if (vim.fn.has("win32") == 0) then
+			nested = io.popen("find " .. path .. " -type f")
+		else
+			nested = io.popen("cd " .. path .. " && dir /s /b | findstr /m /c:.")
+		end
+
+		for n in nested:lines() do
+			if utils.path_exists_in_list(n, ignore_list) ~= 0 then
+				goto continue
+			end
+			if not string.find(string.sub(n, #path + 1, #n), ".git") then
+				selection[#selection + 1] = utils.sub_home_path(n)
+			end
+		end
+
+		::continue::
+	end
+
+	opts = opts or {}
+	pickers.new(opts, {
+		prompt_title = "Open a bookmark",
+		finder = finders.new_table {
+			results = selection
+		},
+		layout_config = {
+			preview_width = 0.5,
+		},
+		sorter = conf.file_sorter(opts),
+		previewer = conf.file_previewer(opts),
+		attach_mappings = function(prompt_bufnr, _)
+			actions.select_default:replace(function()
+				actions.close(prompt_bufnr)
+				open_selection(action_state.get_selected_entry()[1])
+			end)
+			return true
+		end,
+	}):find()
 end
 
-
-local function remove (lines)
-  local selection = action_state.get_selected_entry()
-  if selection ~= nil then
-    local n = ""
-    for k, v in pairs(lines) do
-      if v ~= selection[1] then
-        n = n .. v .. '\n'
-      end
-    end
-    local f = io.open(bookmark_file, 'w')
-    f:write(n)
-    f:close()
-    print("Removed \"" .. selection[1] .. "\" from bookmarks.")
-  end
-end
 
 M.RemoveBookmark = function(opts)
-  local lines = utils.get_lines_from_bookmark_file()
-  if #lines == 0 then
-    print('Bookmark list is empty.')
-    return
-  end
+	local bookmark_list = utils.get_safe_list_from_file(bookmark_file)
+	if #bookmark_list == 0 then
+		print('Bookmark list is empty.')
+		return
+	end
 
-  opts = opts or {}
-  pickers.new(opts, {
-    prompt_title = "Remove a bookmark",
-    finder = finders.new_table {
-      results = lines,
-    },
-    layout_config = {
-      preview_width = 0.5,
-    },
-    sorter = conf.file_sorter(opts),
-    previewer = conf.file_previewer(opts),
-    attach_mappings = function(prompt_bufnr, map)
-      actions.select_default:replace(function()
-        actions.close(prompt_bufnr)
-        remove(lines)
-      end)
-      return true
-    end,
-  }):find()
+	local ignore_list = utils.get_safe_list_from_file(bookmark_file)
+	opts = opts or {}
+	pickers.new(opts, {
+		prompt_title = "Remove a bookmark",
+		finder = finders.new_table {
+			results = bookmark_list,
+		},
+		layout_config = {
+			preview_width = 0.5,
+		},
+		sorter = conf.file_sorter(opts),
+		previewer = conf.file_previewer(opts),
+		attach_mappings = function(prompt_bufnr, _)
+			actions.select_default:replace(function()
+				actions.close(prompt_bufnr)
+				local selection = action_state.get_selected_entry()[1]
+				utils.remove_path_from_list(selection, bookmark_list, bookmark_file)
+				utils.remove_path_from_list(selection, ignore_list, ignore_file)
+				print("Removed \"" .. selection .. "\" from bookmarks.")
+			end)
+			return true
+		end,
+	}):find()
+end
+
+
+M.RemoveIgnore = function(opts)
+	local ignore_list = utils.get_safe_list_from_file(ignore_file)
+	if #ignore_list == 0 then
+		print('Ignore list is empty.')
+		return
+	end
+
+	opts = opts or {}
+	pickers.new(opts, {
+		prompt_title = "Remove an ignore",
+		finder = finders.new_table {
+			results = ignore_list,
+		},
+		layout_config = {
+			preview_width = 0.5,
+		},
+		sorter = conf.file_sorter(opts),
+		previewer = conf.file_previewer(opts),
+		attach_mappings = function(prompt_bufnr, _)
+			actions.select_default:replace(function()
+				actions.close(prompt_bufnr)
+				local selection = action_state.get_selected_entry()[1]
+				utils.remove_path_from_list(selection, ignore_list, ignore_file)
+				print("Removed \"" .. selection .. "\" from ignore list.")
+			end)
+			return true
+		end,
+	}):find()
 end
 
 
--- lua/ezbookmarks/utils.lua
+++ lua/ezbookmarks/utils.lua
@@ -2,74 +2,115 @@
 -- EzBookmarks by lifer0se --
 -----------------------------
 
-local bookmark_file = vim.fn.stdpath('data') .. '/ezbookmarks.txt'
 local u = assert(io.popen("echo $HOME", 'r'))
-local home_path = assert(u:read('*a')):gsub("\n","")
+local home_path = assert(u:read('*a')):gsub("\n", "")
 u:close()
 
 local M = {}
 
-M.get_lines_from_bookmark_file = function ()
-  local lines = {}
-  if vim.fn.filereadable(bookmark_file) == 0 then
-    return lines
-  end
+M.get_user_response = function(message, default)
+	local response = vim.fn.input(message, default)
+	vim.api.nvim_command('normal! :')
+	return response
+end
 
-  local has_changes = false
-  for line in io.lines(bookmark_file) do
-    if vim.fn.filereadable(line) == 1 or vim.fn.isdirectory(line) == 1 then
-      lines[#lines + 1] = line
-    else
-      has_changes = true
-    end
-  end
+M.get_path = function(message)
+	local path = M.get_user_response(message .. ": ", vim.fn.expand("%:p"))
+	if path == nil or path:gsub("%s+", "") == "" then
+		return
+	end
+	if vim.fn.isdirectory(path) == 0 and vim.fn.filereadable(path) == 0 then
+		print("Path \"" .. path .. "\" does not exist.")
+		return
+	end
+	return path
+end
 
-  if has_changes then
-    local n = ""
-    for k, v in pairs(lines) do
-      n = n .. v .. '\n'
-    end
-    local f = io.open(bookmark_file, 'w')
-    f:write(n)
-    f:close()
-  end
+M.path_exists_in_list = function(path, list)
+	for _, v in pairs(list) do
+		if path == v or v:sub(#path + 1) == "/" then
+			return 1
+		elseif path:match(v) then
+			return -1
+		end
+	end
+	return 0
+end
 
-  return lines
+M.try_remove_nested_from_list = function(path, list, file)
+	local n = ""
+	local has_changes = false
+	for _, v in pairs(list) do
+		if not v:find(path) then
+			n = n .. v .. '\n'
+		else
+			has_changes = true
+		end
+	end
+	if has_changes then
+		local f = io.open(file, 'w')
+		f:write(n)
+		f:close()
+	end
+end
+
+M.remove_path_from_list = function(path, list, file)
+	local n = ""
+	for _, v in pairs(list) do
+		if v ~= path then
+			n = n .. v .. '\n'
+		end
+	end
+	local f = io.open(file, 'w')
+	f:write(n)
+	f:close()
+end
+
+M.get_safe_list_from_file = function(file)
+	local list = {}
+	if vim.fn.filereadable(file) == 0 then
+		return list
+	end
+
+	local has_changes = false
+	for line in io.lines(file) do
+		if vim.fn.filereadable(line) == 1 or vim.fn.isdirectory(line) == 1 then
+			list[#list + 1] = line
+		else
+			has_changes = true
+		end
+	end
+
+	if has_changes then
+		local n = ""
+		for _, v in pairs(list) do
+			n = n .. v .. '\n'
+		end
+		local f = io.open(file, 'w')
+		f:write(n)
+		f:close()
+	end
+
+	return list
 end
 
 
-M.bookmark_exists = function (path)
-  local lines = M.get_lines_from_bookmark_file()
-  for k, v in pairs(lines) do
-    if (vim.fn.filereadable(v) == 1 or vim.fn.isdirectory(v) == 1) and v == path then
-      return 1
-    elseif vim.fn.filereadable(v) == 1 then
-      for k, v in pairs(lines) do
-        if string.sub(v, #v) == "/" and string.match(path, v) and v ~= path then
-          return -1
-        end
-      end
-    elseif vim.fn.isdirectory(v) == 1 and string.match(path, v) then
-      return -1
-    end
-  end
-  return 0
+M.sub_home_path = function(file)
+	if string.sub(file, 0, #home_path) == home_path then
+		return "~" .. string.sub(file, #home_path + 1, #file)
+	else
+		return file
+	end
 end
 
-M.sub_home_path = function (file)
-  if string.sub(file, 0, #home_path) == home_path then
-    return "~" .. string.sub(file, #home_path + 1, #file)
-  else
-    return file
-  end
+
+M.get_path_from_file = function(file)
+	if (vim.fn.has("win32") == 0) then
+		return file:match("(.*/)")
+	else
+		return file:match("(.*\\)")
+	end
 end
 
-M.get_path_from_file = function (file)
-  if (vim.fn.has("win32") == 0) then
-    return file:match("(.*/)")
-  else
-    return file:match("(.*\\)")
-  end
-end
 
 return M
